5407db9f085ad5f6f306a37d9a4e914b0632b386
soondong2 <100760303+soondong2@users.noreply.github.com>
2022-07-14 15:58:38 +0900

    Create EDA.py

---
+++ pages/EDA.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from html_module import section, callout, line_break, title
+
+st.set_page_config(
+    page_title="소비자 물가지수 EDA",
+    page_icon="📈",
+)
+title('지출 목적별 소비자 물가지수 EDA')
