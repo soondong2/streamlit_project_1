ed76a6f6a26e2fa156e3eedc5a83aba4c644ae20
soondong2 <100760303+soondong2@users.noreply.github.com>
2022-07-14 16:15:13 +0900

    Update EDA.py

--- pages/EDA.py
+++ pages/EDA.py
@@ -61,10 +61,8 @@ def CPI_plot(data, code, region):
     plt.show()
 
 # 대분류 품목별 물가지수 plot
- options = st.multiselect(
-     'What are your wnat category and region?',
+option = st.selectbox(
+     'What are your wnat category?',
      ['0', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12],
-     ['전국', '서울특별시', '울산광역시', '인천광역시', '광주광역시', '대구광역시', '대전광역시', '부산광역시', '세종특별자치시', '제주특별자치도', '강원도', '경기도',
-     '경상남도', '경상북도', '전라남도', '전라북도'])
+     )
 
-st.write('You selected:', options)
