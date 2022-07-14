a69787368b0b863a5a7cac26048621c45bf07c51
soondong2 <100760303+soondong2@users.noreply.github.com>
2022-07-14 17:12:08 +0900

    Update EDA.py

--- pages/EDA.py
+++ pages/EDA.py
@@ -89,3 +89,8 @@ option2 = st.selectbox(
      ('전국', '서울특별시', '울산광역시', '인천광역시', '광주광역시', '대구광역시', '대전광역시', '부산광역시', '세종특별자치시', '제주특별자치도', '강원도', '경기도',
      '경상남도', '경상북도', '전라남도', '전라북도')
      )
+
+option3 = st.multiselect(
+     'What are your favorite colors',
+     ['Green', 'Yellow', 'Red', 'Blue'],
+     ['Yellow', 'Red'])
